63a0b044c31189fd1ab39b5d0ae95daadc64a761
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-30 00:52:45 +0300

    edit the firebase api key

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-f6513.firebaseapp.com",
   projectId: "twitter-f6513",
   storageBucket: "twitter-f6513.appspot.com",
