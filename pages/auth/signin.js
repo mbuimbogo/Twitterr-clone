aa0bf303973b04625e9304453a163b7600bf4acc
mbuimbogo <mbuimbogo99@gmail.com>
2023-04-02 21:39:01 +0300

    Remove authentication by next-auth

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,6 +1,10 @@
-import { getProviders, signIn } from "next-auth/react"
 
-export default function Signin({ providers }) {
+
+export default function Signin() {
+
+const onGoogleClick = async()=>{
+  
+}
     
   return (
     <div className="flex justify-center mt-20 space-x-4">
@@ -10,25 +14,16 @@ export default function Signin({ providers }) {
         className="hidden object-cover md:w-44 md:h-80 rotate-6  md:inline-flex"
       />
       <div className="">
-        {Object.values(providers).map((provider) =>
-        <div key={provider.name} className="flex flex-col items-center"> 
+        
+        <div  className="flex flex-col items-center"> 
             <img src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png" alt="twitter logo" className="w-36 object-cover"/>
             <p className="text-center text-sm italic my-10">This app is created for learning purposes</p>
-            <button onClick={()=> signIn(provider.id, {callbackUrl:"/"})} className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500">Sign in with {provider.name}</button>
+            <button onClick={onGoogleClick} className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500">Sign in with Google</button>
         </div>
-        )}
+        
       </div>
     </div>
   )
 }
 
-export async function getServerSideProps(){
-    const providers = await getProviders();
-  
-    return {
-        props:{
-            providers,
-        },
-    }
-}
 
